5a17976048079271d2c7b7f064ea7d56bb3b0c32
Atul Kumar Hande <handeatul03a@gmail.com>
2021-10-03 22:25:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Setup from "./setup";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Setup />
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const data = [
+  { id: 1, name: "chris" },
+  { id: 2, name: "ram" },
+  { id: 3, name: "chipper" },
+  { id: 4, name: "nemises" }
+];
